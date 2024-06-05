21d043069bf0695795b9b46612fd6e1ffc929182
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-05 16:41:05 +0900

    [Setting] LottoLog 환경 설정

---
+++ LottoLog/LottoLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoLog
+//
+//  Created by Jisoo HAM on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
