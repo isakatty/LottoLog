674c0e56953ec88d98498c6b95888f1da0520d59
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-06 01:41:32 +0900

    [Add] TextField와 PickerView를 통해 최신회차 로또 데이터 가져와 화면 그리기

--- LottoLog/LottoLog/ViewController.swift
+++ LottoLog/LottoLog/ViewController.swift
@@ -12,6 +12,17 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    lazy var chooseTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "회차를 입력하세요."
+        tf.borderStyle = .roundedRect
+        tf.inputView = datePicker
+        tf.tintColor = .clear
+        return tf
+    }()
+    
+    let datePicker = UIPickerView()
+    
     let numberStackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
@@ -20,7 +31,6 @@ class ViewController: UIViewController {
         stack.spacing = 7
         return stack
     }()
-    
     var drawNo1 = CircleView()
     var drawNo2 = CircleView()
     var drawNo3 = CircleView()
@@ -37,16 +47,20 @@ class ViewController: UIViewController {
     
     let descriptionView = DescriptionView()
     
+    // pickerview에서 선택할 요소인데, 값이 바뀔때 어떻게 가져올 수 있지 ?
+    let list: [String] = ["1110","1111","1112","1113","1114","1115","1116","1117","1118","1119", "1120", "1121", "1122"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        networking()
+        
         configureHierarchy()
         configureLayout()
         configureUI()
+        configurePicker()
     }
     
-    func networking() {
-        let url = privateKey.lottoURL + "1107"
+    func networking(selected: String) {
+        let url = privateKey.lottoURL + selected
         
         AF.request(url).responseDecodable(of: Lotto.self) { response in
             switch response.result {
@@ -81,10 +95,9 @@ class ViewController: UIViewController {
             }
         }
     }
-    
     func configureHierarchy() {
         
-        [descriptionView, numberStackView]
+        [chooseTextField, descriptionView, numberStackView]
             .forEach { view.addSubview($0) }
         
         [drawNo1, drawNo2, drawNo3, drawNo4,
@@ -95,8 +108,14 @@ class ViewController: UIViewController {
     func configureLayout() {
         let safeArea = view.safeAreaLayoutGuide
         
+        chooseTextField.snp.makeConstraints { make in
+            make.top.equalTo(safeArea).offset(30)
+            make.leading.trailing.equalTo(safeArea).inset(15)
+            make.height.equalTo(40)
+        }
+        
         descriptionView.snp.makeConstraints { make in
-            make.top.equalTo(safeArea).offset(100)
+            make.top.equalTo(chooseTextField.snp.bottom).offset(30)
             make.centerX.equalTo(safeArea)
             make.leading.trailing.equalTo(safeArea).inset(15)
             make.width.greaterThanOrEqualTo(80)
@@ -113,6 +132,41 @@ class ViewController: UIViewController {
     func configureUI() {
         view.backgroundColor = .systemBackground
     }
+    
+    func configurePicker() {
+        datePicker.delegate = self
+        datePicker.dataSource = self
+    }
 
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        numberOfRowsInComponent component: Int
+    ) -> Int {
+        return list.count
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        titleForRow row: Int,
+        forComponent component: Int
+    ) -> String? {
+        return "\(list[row]) 회차"
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        didSelectRow row: Int,
+        inComponent component: Int
+    ) {
+        networking(selected: list[row])
+        chooseTextField.text = list[row] + " 회차"
+    }
 }
 
